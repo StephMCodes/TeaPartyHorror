23eee90976306096412e7713ed6cec75fe3c19b6
Felix Soumpholphakdy <felix@felixsoum.com>
2024-04-02 13:54:27 -0400

    Add hello world

--- TeaPartyHorror_Game/Program.cs
+++ TeaPartyHorror_Game/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TeaPartyHorror_Game
 {
@@ -10,6 +6,8 @@ namespace TeaPartyHorror_Game
     {
         static void Main(string[] args)
         {
+            // From Felix
+            Console.WriteLine("Hello World");
         }
     }
 }
