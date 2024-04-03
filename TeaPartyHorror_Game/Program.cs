65cad1702c35c83f906ffd35a7976fae737d8d40
Stephanie M <shelovesgames965@gmail.com>
2024-04-03 09:59:02 -0400

    Continuation of save features

--- TeaPartyHorror_Game/Program.cs
+++ TeaPartyHorror_Game/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.IO;
+using
+
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace TeaPartyHorror_Game
 {
     internal class Program
     {
+        [Serializable]
+        public class SaveData
+        {
+            public int numbertToSave;
+            public string stringToSave;
+
+            public SaveData(int numbertToSave, string stringToSave)
+            {
+                this.numbertToSave = numbertToSave;
+                this.stringToSave = stringToSave;
+            }
+        }
+
+        static SaveData savedata;
         static void Main(string[] args)
         {
 
-            if (!File.Exists("Save.txt"))
-            { File.CreateText("Save.txt"); }
-            File.WriteAllText("Save.txt", "Hello World");
+            const string SaveFile = "Save.txt";
+            if (!File.Exists(SaveFile))
+            { File.CreateText(SaveFile); }
+
+            //savedata = new SaveData(200, "Felix");
+            var bf = new BinaryFormatter();
+            //bf.Serialize(File.OpenWrite(SaveFile), savedata);
+            savedata=bf.Deserialize(File.OpenRead(SaveFile)) as SaveData;
+
             // From Felix
             Console.WriteLine("Hello World");
             //BELLA IS STRUGGLING no more
