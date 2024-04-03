1e45435017a4e4e5f1ccbc424e47cd23dfa489bf
Stephanie M <shelovesgames965@gmail.com>
2024-04-03 09:33:20 -0400

    Creating basis for saving function

--- TeaPartyHorror_Game/Program.cs
+++ TeaPartyHorror_Game/Program.cs
@@ -6,6 +6,10 @@ namespace TeaPartyHorror_Game
     {
         static void Main(string[] args)
         {
+
+            if (!File.Exists("Save.txt"))
+            { File.CreateText("Save.txt"); }
+            File.WriteAllText("Save.txt", "Hello World");
             // From Felix
             Console.WriteLine("Hello World");
             //BELLA IS STRUGGLING no more
