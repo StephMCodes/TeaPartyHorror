1f31f17afeef4d63479d67527b0d5eec7cb0ba8a
Bella <bellaperezppg@gmail.com>
2024-04-12 14:25:49 -0400

    Fixed nesting issue use a method and if else statements instead

--- TeaPartyHorror_Game/Rooms/DiningRoom.cs
+++ TeaPartyHorror_Game/Rooms/DiningRoom.cs
@@ -14,38 +14,76 @@ namespace TeaPartyHorror_Game.Rooms
 
         internal override void ReceiveChoice(string choice)
         {
-            
-            switch (choice)
+            if (choice == "1")
             {
-                case "1": //begin minigame
-                    Console.Clear();
-                    Console.WriteLine("Choice [good1] for good [bad1] for bad");
-                    switch (diningChoice1) //bella says to use if elses etc careful with defaults, make sure they are
-                        //allowed to make spelling mistakes and simply restart the q they were on and not the whole game
-                    {
-                        case "good1":
-                            Console.WriteLine("Ghost is happy. Good!");
-                            break;
-                        case "bad1":
-                            Console.WriteLine("Ghost is mad! EEP!");
-                            Game.IncreaseFear(1);
-                            break;
-                        //default:
-                           // Console.WriteLine("Invalid command.");
-                            //break;
-                    } //end of minigame
-                    Console.WriteLine("Choice 1 for good 2 for bad");
-                    break;
-                case "hallway":
-                    Game.Transition<Hallway>();
-                    break;
-                default:
-                    Console.WriteLine("Invalid command.");
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine("Press 1 to sit down");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
+                StartMiniGame();
             }
+            else if (choice == "hallway")
+            {
+                Game.Transition<Hallway>();
+            }
+            else
+            {
+                Console.WriteLine("Invalid command.");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Press 1 to sit down");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            //switch (choice)
+            //{
+            //    case "1": //begin minigame
+            //        Console.Clear();
+            //        Console.WriteLine("Choice [good1] for good [bad1] for bad");
+            //        switch (diningChoice1) //bella says to use if elses etc careful with defaults, make sure they are
+            //            //allowed to make spelling mistakes and simply restart the q they were on and not the whole game
+            //        {
+
+            //            case "good1":
+            //                Console.WriteLine("Ghost is happy. Good!");
+            //                break;
+            //            case "bad1":
+            //                Console.WriteLine("Ghost is mad! EEP!");
+            //                Game.IncreaseFear(1);
+            //                break;
+            //            //default:
+            //               // Console.WriteLine("Invalid command.");
+            //                //break;
+            //        } //end of minigame
+            //        Console.WriteLine("Choice 1 for good 2 for bad");
+            //        break;
+            //    case "hallway":
+            //        Game.Transition<Hallway>();
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid command.");
+            //        Console.ForegroundColor = ConsoleColor.Magenta;
+            //        Console.WriteLine("Press 1 to sit down");
+            //        Console.ForegroundColor = ConsoleColor.White;
+            //        break;
+            //}
+        }
+        private void StartMiniGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Choice [good1] for good [bad1] for bad");
+            string miniGameChoice = Console.ReadLine();
+
+            if (miniGameChoice == "good1")
+            {
+                Console.WriteLine("Ghost is happy. Good! ");
+            }
+            else if (miniGameChoice == "bad1")
+
+            {
+                Console.WriteLine("Ghost is mad! EEP!");
+                Game.IncreaseFear(1);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command.Try again! ");
+            }
+
         }
 
     }
