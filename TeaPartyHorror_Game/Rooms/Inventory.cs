cb598b5543ee292a77cda71fa028454cb93302b8
Bella <bellaperezppg@gmail.com>
2024-04-15 19:55:10 -0400

    fixed with commented out where needed

--- TeaPartyHorror_Game/Rooms/Inventory.cs
+++ TeaPartyHorror_Game/Rooms/Inventory.cs
@@ -4,75 +4,75 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using static TeaPartyHorror_Game.Rooms.Inventory;
+//using static TeaPartyHorror_Game.Rooms.Inventory;
 
-namespace TeaPartyHorror_Game.Rooms
-{
-    internal class Inventory
-    {
+//namespace TeaPartyHorror_Game.Rooms
+//{
+    //internal class Inventory
+    //{
 
-        public abstract class Item
-        {
-            public abstract string Name { get; protected set; }
-            public abstract void Use();
-        }
-        public class Snack : Item
-        {
-            private static readonly string[] snackTypes = { "strawberry ", "cookie ", "brownie " };
-            private static readonly Random rnd = new Random();
+    //    public abstract class Item
+    //    {
+    //        public abstract string Name { get; protected set; }
+    //        public abstract void Use();
+    //    }
+    //    public class Snack : Item
+    //    {
+    //        private static readonly string[] snackTypes = { "strawberry ", "cookie ", "brownie " };
+    //        private static readonly Random rnd = new Random();
 
-            public Snack()
-            {
-                Random rnd = new Random();
-                Name = snackTypes[rnd.Next(snackTypes.Length)] + "snack ";
-            }
+    //        public Snack()
+    //        {
+    //            Random rnd = new Random();
+    //            Name = snackTypes[rnd.Next(snackTypes.Length)] + "snack ";
+    //        }
 
-            public override string Name { get; protected set; }
-            public override void Use()
-            {
-                Console.WriteLine($"Using{Name}.");
-            }
-            else
-            {
-                Console.WriteLine("Item not found in inventory.");
-            }             
-        }
-        public class Amulet : Item
-        {
-            public Amulet()
-            {
-                Name = "Amulet";
-            }
-            public override string Name { get; protected set; }
-            public override void Use()
-            {
-                Console.WriteLine("Using Amulet to reduce fear.");
-            }
-        }
-        public class Bouquet : Item
-        {
-            public Bouquet()
-            {
-                Name = "Bouquet";
-            }
-            public override string Name { get; protected set; }
-            public override void Use()
-            {
-                Console.WriteLine(" Item to give the lady ");
-            }
-        }
-        public class MrBunnyRabbit : Item
-        {
-            public MrBunnyRabbit()
-            {
-                Name = "Bunny-Rabbit";
-            }
-            public override string Name { get; protected set; }
-            public override void Use()
-            {
-                Console.WriteLine("Mr.Bunny-Rabbit says, 'You are uninvited to my birthday party you mere mortal!' ");
-            }
-        }
+    //        public override string Name { get; protected set; }
+    //        public override void Use()
+    //        {
+    //            Console.WriteLine($"Using{Name}.");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Item not found in inventory.");
+    //        }             
+    //    }
+    //    public class Amulet : Item
+    //    {
+    //        public Amulet()
+    //        {
+    //            Name = "Amulet";
+    //        }
+    //        public override string Name { get; protected set; }
+    //        public override void Use()
+    //        {
+    //            Console.WriteLine("Using Amulet to reduce fear.");
+    //        }
+    //    }
+    //    public class Bouquet : Item
+    //    {
+    //        public Bouquet()
+    //        {
+    //            Name = "Bouquet";
+    //        }
+    //        public override string Name { get; protected set; }
+    //        public override void Use()
+    //        {
+    //            Console.WriteLine(" Item to give the lady ");
+    //        }
+    //    }
+    //    public class MrBunnyRabbit : Item
+    //    {
+    //        public MrBunnyRabbit()
+    //        {
+    //            Name = "Bunny-Rabbit";
+    //        }
+    //        public override string Name { get; protected set; }
+    //        public override void Use()
+    //        {
+    //            Console.WriteLine("Mr.Bunny-Rabbit says, 'You are uninvited to my birthday party you mere mortal!' ");
+    //        }
+    //    }
 
         //public class Inventory
         //{
@@ -113,9 +113,9 @@ namespace TeaPartyHorror_Game.Rooms
 
         //}
 
-    }
-}
-=======
+//    }
+//}
+//=======
 //using static TeaPartyHorror_Game.Rooms.Inventory;
 
 //namespace TeaPartyHorror_Game.Rooms
@@ -223,4 +223,4 @@ namespace TeaPartyHorror_Game.Rooms
 
 //    }
 //}
->>>>>>> 27fc3eef11d467108d4db07f65269c531b379bad
+// >>>>>>> 27fc3eef11d467108d4db07f65269c531b379bad
