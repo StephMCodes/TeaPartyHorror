865e3bddb5527c08621d37d0da672e1a8e7c3c3b
Stephanie M <shelovesgames965@gmail.com>
2024-04-18 12:54:36 -0400

    Continue the boss fight, added an ending and end room

---
+++ TeaPartyHorror_Game/Rooms/End.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaPartyHorror_Game.Rooms
+{
+    internal class End : Room
+    {
+        internal override string CreateDescription()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            return "\nTHANK YOU FOR PLAYING TEA PARTY HORROR\nCreators:\nBELLA PEREZ\nSTEPHANIE MICHIU";
+
+
+        }
+
+
+        internal override void ReceiveChoice(string choice)
+        {
+            Game.Finish();
+        }
+    }
+}
